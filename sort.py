9c763dc6e507718f76e163ce6ff3fbcdfa8d353c
Konstantin Krastev <grizmin@gmail.com>
2015-11-09 20:20:42 +0200

    code optimized

--- sort.py
+++ sort.py
@@ -4,14 +4,9 @@ def create_file_list(CWD):
     """ takes string as path, returns tuple(files,date) """
 
     files_with_mtime = []
-    files = []
-    for filename in os.listdir(CWD):
-        if os.path.splitext(os.path.join(CWD,filename))[1] in ext:
-            files.append(filename)
-    for fi in files:
-        files_with_mtime.append((fi,datetime.datetime.fromtimestamp(os.stat(fi).st_mtime).strftime('%Y-%m-%d')))
+    for filename in [f for f in os.listdir(CWD) if os.path.splitext(f)[1] in ext]:
+        files_with_mtime.append((filename,datetime.datetime.fromtimestamp(os.stat(filename).st_mtime).strftime('%Y-%m-%d')))
     return files_with_mtime
-
 
 def create_directories(files):
     """ takes tuple(file,date) from create_file_list() """
@@ -29,11 +24,10 @@ def create_directories(files):
 def move_files_to_folders(files):
     """ gets tuple(file,date) from create_file_list() """
     for i in files:
-        if os.path.exists(os.path.join(CWD,i[1])):
-            try:
-                os.rename(os.path.join(CWD,i[0]), os.path.join(CWD,(i[1] + '/' + i[0])))
-            except Exception as e:
-                raise 
+        try:
+            os.rename(os.path.join(CWD,i[0]), os.path.join(CWD,(i[1] + '/' + i[0])))
+        except Exception as e:
+            raise
     return len(files)
 
 
@@ -42,11 +36,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog=sys.argv[0], usage='%(prog)s [options]')
     parser.add_argument("-e","--extension",action='append',help="File extensions to match",required=True)
     args = parser.parse_args()
-  
+
     ext =  ['.' + e for e in args.extension]
     print "Moving files with extensions:", ext
     CWD = os.getcwd()
     files = create_file_list(CWD)
     create_directories(files)
     print "Moved %i files" % move_files_to_folders(files)
-
